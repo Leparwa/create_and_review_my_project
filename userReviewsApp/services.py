58acb48535b3bde865d65f4538fbbe7dc32ec8c0
leparwa <kevin.leparwa@student.moringaschool.com>
2021-10-02 04:38:22 +0300

    changed url

--- userReviewsApp/services.py
+++ userReviewsApp/services.py
@@ -8,30 +8,30 @@ from django.core import serializers
 api_base_url='https://prorater-api.herokuapp.com/'
 
 def register_user(data):
-    response=requests.post('http://127.0.0.1:4000/user/register',json=data) 
+    response=requests.post('https://prorater-api.herokuapp.com/user/register',json=data) 
     print(response.json)
     return response.json()
 def login_user(data):
-    response=requests.post('http://127.0.0.1:4000/user/login', json=data) 
+    response=requests.post('https://prorater-api.herokuapp.com/user/login', json=data) 
     print(response.json())
     return response.json()
 def get_user(data):
-    response=requests.get('http://127.0.0.1:4000/user') 
+    response=requests.get('https://prorater-api.herokuapp.com/user') 
     print(response)
     return response
 def get_user_profile(data):
-    response=requests.get('http://127.0.0.1:4000/user/profile',json=data) 
+    response=requests.get('https://prorater-api.herokuapp.com/user/profile',json=data) 
     print(response.json)
 def create_use_project(data):
     print(data)
-    response=requests.post('http://127.0.0.1:4000/project/',data) 
+    response=requests.post('https://prorater-api.herokuapp.com/project/',data) 
     return response
 def get_user_projects():
-    all_projects=requests.get('http://127.0.0.1:4000/project')
+    all_projects=requests.get('https://prorater-api.herokuapp.com/project')
     user_data = all_projects.json()
     return user_data
 def get_all_projects():
-    response=requests.get('http://127.0.0.1:4000/user/projects') 
+    response=requests.get('https://prorater-api.herokuapp.com/user/projects') 
     data =response.json()
     return data
    
